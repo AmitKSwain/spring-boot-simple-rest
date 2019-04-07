d453d11193e12f42cd075188c67ea496018f7bc8
AmitKSwain <amit.here1@gmail.com>
2019-04-07 17:06:22 +0530

    Code added for taking parameters through path variables

--- spring-boot-simple-rest/src/main/java/com/practice/springboot/rest/simple/controller/UerController.java
+++ spring-boot-simple-rest/src/main/java/com/practice/springboot/rest/simple/controller/UerController.java
@@ -1,6 +1,7 @@
 package com.practice.springboot.rest.simple.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,10 +17,19 @@ public class UerController {
 		return "OK";
 	}
 	
+	//pass parameters using query params
 	@GetMapping("/greet")
 	public String greet(@RequestParam("name") String name){
 		return "Hello "+name;
 	}
+	
+	//pass parameters using path variable
+	@GetMapping("/bye/{name}")
+	public String sayBye(@PathVariable("name") String name){
+		return "Bye "+name;
+	}
+	
+	//inpost using request body
 	@PostMapping("/register")
 	public String registerUser(@RequestBody User user){
 		return "User :"+user.getName()+" is registered .";
